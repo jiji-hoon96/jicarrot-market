42eee0cfeb92f1f0691db860280c049bf29d5a43
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-06-16 22:18:44 +0900

    Revert "aㄴㅇㄹㄴㅇㅁㅁㄴㅇㅁㄹㄴㅇㄹㅇㄴ"
    This reverts commit dc549942382b99e7fc39ef056b25b72306be95bc, reversing
    changes made to 5b007bd4903a27fab351c4986581ad6dd3f3ce5f.

--- libs/client.ts
+++ libs/client.ts
@@ -1,11 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
 <<<<<<< HEAD
-<<<<<<< HEAD
 const client = new PrismaClient();
-=======
-export default new PrismaClient();
->>>>>>> 974160d1a927a87e92ccd42ebe787d37790f39b3
 =======
 export default new PrismaClient();
 >>>>>>> 974160d1a927a87e92ccd42ebe787d37790f39b3
