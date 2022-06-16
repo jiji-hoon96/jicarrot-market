52d6bfdbe1df46f85c51607a24636d7490783809
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-06-16 23:02:20 +0900

    16. prisma 재설정 완료

--- libs/client.ts
+++ libs/client.ts
@@ -1,7 +1,3 @@
 import { PrismaClient } from "@prisma/client";
 
-<<<<<<< HEAD
 const client = new PrismaClient();
-=======
-export default new PrismaClient();
->>>>>>> 974160d1a927a87e92ccd42ebe787d37790f39b3
