b7228ea98bdb0ff292119d8134789ffd07907dd3
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-06-17 13:50:31 +0900

    17-1 useForm 이용해 input 정리 / 데이터 fetch

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,13 +1,37 @@
 import type { NextPage } from "next";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 import Button from "../components/button";
 import Input from "../components/input";
 import { cls } from "../libs/utils";
 
+interface EnterForm{
+  email?: string;
+  phone?: string;
+}
+
 const Enter: NextPage = () => {
+  const [submitting, setSubmitting] = useState(false);
+  const {register,handleSubmit,reset} = useForm<EnterForm>()
   const [method, setMethod] = useState<"email" | "phone">("email");
-  const onEmailClick = () => setMethod("email");
-  const onPhoneClick = () => setMethod("phone");
+  const onEmailClick = () => {
+    reset();
+    setMethod("email");
+}
+  const onPhoneClick = () => {
+    reset();
+    setMethod("phone");
+}
+  const onValid = (data:EnterForm)=>{
+    setSubmitting(true)
+    fetch("/api/users/enter",{
+      method: "POST",
+      body: JSON.stringify(data),
+      headers:{
+        "Content-Type" : "application/json",
+      }
+    }).then(()=>{setSubmitting(false)});
+  }
   return (
     <div className="mt-16 px-4">
       <h3 className="text-3xl font-bold text-center">Enter to Carrot</h3>
@@ -39,12 +63,17 @@ const Enter: NextPage = () => {
             </button>
           </div>
         </div>
-        <form className="flex flex-col mt-8 space-y-4">
+        <form className="flex flex-col mt-8 space-y-4" onSubmit={handleSubmit(onValid)}>
           {method === "email" ? (
-            <Input name="email" label="Email address" type="email" required />
+            <Input register={register("email" ,{
+              required : true,
+            })} name="email" label="Email address" type="email" required />
           ) : null}
           {method === "phone" ? (
             <Input
+              register={register("phone",{
+                required : true,
+              })} 
               name="phone"
               label="Phone number"
               type="number"
@@ -52,9 +81,9 @@ const Enter: NextPage = () => {
               required
             />
           ) : null}
-          {method === "email" ? <Button text={"Get login link"} /> : null}
+          {method === "email" ? <Button text={submitting ? "로딩중" : "Get login link"} /> : null}
           {method === "phone" ? (
-            <Button text={"Get one-time password"} />
+            <Button text={submitting ? "로딩중" : "Get one-time password"} />
           ) : null}
         </form>
 
