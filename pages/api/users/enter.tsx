775aabcb3d1a663c6d470826702991e0883f1f63
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-06-19 22:38:00 +0900

    19-2 token 생성

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,26 +1,28 @@
-import withHandler from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
-import {client} from '@libs/server/client'
+import withHandler from "@libs/server/withHandler";
+import {client} from "@libs/server/client";
 
-
-async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const {phone,email} = req.body;
-  const payload = phone ? {phone : +phone} : {email}
-  const user = await client.user.upsert({
-    where: {
-      ...payload
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { phone, email } = req.body;
+  const user = phone ? { phone: +phone } : { email };
+  const payload = Math.floor(100000 + Math.random() * 900000) + "";
+  const token = await client.token.create({
+    data: {
+      payload,
+      user: {
+        connectOrCreate: {
+          where: {
+            ...user,
+          },
+          create: {
+            name: "Anonymous",
+            ...user,
+          },
+        },
+      },
     },
-    create: {
-      name: "익명",
-      ...payload
-    },
-    update: {},
-  })
-  console.log(user)
+  });
+  console.log(token);
   return res.status(200).end();
 }
-
-export default withHandler("POST",handler)
+export default withHandler("POST", handler);
