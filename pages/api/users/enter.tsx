90fa8ca41f2b80fbc820643a14ed6a3ebd281a6d
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-06-20 17:34:19 +0900

    20-2 email 로 토큰 보내고 받기

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,8 +1,10 @@
+import mail from "@sendgrid/mail";
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import {client} from "@libs/server/client";
 import twilio from 'twilio';
 
+mail.setApiKey(process.env.MY_EMAIL!);
 const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
 async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
@@ -33,6 +35,15 @@ async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) 
       body: `휴대폰 로그인을 위한 Token 은  ${payload} 입니다`,
     });
     console.log(message)
+  }else if(email){
+    const email = await mail.send({
+      from: "lsy8469@naver.com",
+      to: "lsy8469@naver.com",
+      subject:"Your Jicarrot market Verification Email",
+      text:`이메일 로그인을 위한 Token 은  ${payload} 입니다`,
+      html: `<strong>이메일 로그인을 위한 Token 은  ${payload} 입니다</strong>`,
+    });
+    console.log(email)
   }
   return res.json({
     ok:true
